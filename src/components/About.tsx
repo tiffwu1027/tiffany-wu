2daa6e396691e50945cf42b9c5fd1313e023063b
Tiffany <tiffwu1027@gmail.com>
2021-02-27 23:58:39 -0800

    added logo test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import headShot from '../images/headshot.jpg';
 import '../styling/About.css';
+import eyeLogo from '../images/eye_logo.svg';
+import SVG from 'react-inlinesvg';
 
 
 interface Props {
@@ -23,6 +25,8 @@ export const About: React.FC<Props> = () => {
           <h3>Tiffany Wu</h3>
           <p>Hi there! I am an aspiring vision science researcher, currently working with <a href="https://psych.ubc.ca/profile/ronald-rensink/">Dr. Rensink</a> in the <a href="https://viscoglab.psych.ubc.ca/">UBC Visual Cognition Lab</a>. I am interested in the intersection between computer science and psychology, specifically in relation to modelling visual attention.</p>
       </div>
+
+      <SVG src={eyeLogo} />
    </div>
     );
 }
--- src/components/VCLit.tsx
+++ src/components/VCLit.tsx
@@ -7,7 +7,8 @@ interface VCLitProps {
 export const VCLit: React.FC<VCLitProps> = () => {
         return (
             <div>
-                IT
+                <h3>IT</h3>
+                <p>Using visual search with a shaker paradigm to investigate our mental representations of space and item size.</p>
             </div>
         );
 }

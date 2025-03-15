79050de99daf78deabf22cf7fcfe89fd98ec58a8
Tiffany <tiffwu1027@gmail.com>
2025-03-15 09:16:19 -0400

    add package json changes for deploying

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HjbBqYlEm9dUXqBo5wh8AItNqdY8NkjIs/rI6y6H6Wo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/tiffany-wu",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
